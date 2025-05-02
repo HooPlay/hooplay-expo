27ddf2583de6d8304d665ee8648048e9515cf7e7
SeongOh Park <qkrtjddh1212@naver.com>
2025-05-02 17:41:32 +0900

    📦 Install Packages
    - nativewind
    - rn-primitives

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,30 +1,78 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import '~/global.css';
+
+import { DarkTheme, DefaultTheme, Theme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import { useColorScheme } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { Platform } from 'react-native';
+import { NAV_THEME } from '~/lib/constants';
+import { useColorScheme } from '~/lib/useColorScheme';
+
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+};
+
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const hasMounted = React.useRef(false);
+  const { colorScheme, isDarkColorScheme } = useColorScheme();
+  const [isColorSchemeLoaded, setIsColorSchemeLoaded] = React.useState(false);
+
   const [loaded] = useFonts({
-    'Pretendard-Black': require('@/assets/fonts/Pretendard-Black.ttf'),
-    'Pretendard-Bold': require('@/assets/fonts/Pretendard-Bold.ttf'),
-    'Pretendard-ExtraBold': require('@/assets/fonts/Pretendard-ExtraBold.ttf'),
-    'Pretendard-ExtraLight': require('@/assets/fonts/Pretendard-ExtraLight.ttf'),
-    'Pretendard-Light': require('@/assets/fonts/Pretendard-Light.ttf'),
-    'Pretendard-Medium': require('@/assets/fonts/Pretendard-Medium.ttf'),
-    'Pretendard-Regular': require('@/assets/fonts/Pretendard-Regular.ttf'),
-    'Pretendard-SemiBold': require('@/assets/fonts/Pretendard-SemiBold.ttf'),
-    'Pretendard-Thin': require('@/assets/fonts/Pretendard-Thin.ttf'),
+    'Pretendard-Black': require('~/assets/fonts/Pretendard-Black.ttf'),
+    'Pretendard-Bold': require('~/assets/fonts/Pretendard-Bold.ttf'),
+    'Pretendard-ExtraBold': require('~/assets/fonts/Pretendard-ExtraBold.ttf'),
+    'Pretendard-ExtraLight': require('~/assets/fonts/Pretendard-ExtraLight.ttf'),
+    'Pretendard-Light': require('~/assets/fonts/Pretendard-Light.ttf'),
+    'Pretendard-Medium': require('~/assets/fonts/Pretendard-Medium.ttf'),
+    'Pretendard-Regular': require('~/assets/fonts/Pretendard-Regular.ttf'),
+    'Pretendard-SemiBold': require('~/assets/fonts/Pretendard-SemiBold.ttf'),
+    'Pretendard-Thin': require('~/assets/fonts/Pretendard-Thin.ttf'),
   });
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
+  useIsomorphicLayoutEffect(() => {
+    if (hasMounted.current) {
+      return;
+    }
+
+    if (Platform.OS === 'web') {
+      // Adds the background color to the html element to prevent white background on overscroll.
+      document.documentElement.classList.add('bg-background');
+    }
+    setIsColorSchemeLoaded(true);
+    hasMounted.current = true;
+  }, []);
+
+  if (!isColorSchemeLoaded || !loaded) {
     return null;
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack />
+    <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+      <StatusBar style={isDarkColorScheme ? 'light' : 'dark'} />
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            title: 'Starter Base',
+          }}
+        />
+      </Stack>
     </ThemeProvider>
   );
 }
+
+const useIsomorphicLayoutEffect =
+  Platform.OS === 'web' && typeof window === 'undefined'
+    ? React.useEffect
+    : React.useLayoutEffect;
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
+import { Button } from '~/components/ui/button';
+import '../global.css';
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button variant={'destructive'}>Test Button</Button>
+      <Text className="text-2xl">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
